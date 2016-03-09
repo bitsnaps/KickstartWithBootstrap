1b7bb92f9092569c143e2a35bba69d191f25b6d4
bitsnaps <bitsnaps@yahoo.fr>
2016-03-09 17:20:23 +0100

    updated to default template for 2.4.4

--- src/templates/artifacts/TagLib.groovy
+++ src/templates/artifacts/TagLib.groovy
@@ -3,6 +3,6 @@
  * A taglib library provides a set of reusable tags to help rendering the views.
  */
 class @artifact.name@ {
-    static defaultEncodeAs = 'html'
-    //static encodeAsForTags = [tagName: 'raw']
+    static defaultEncodeAs = [taglib:'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
 }
